7be42b92d20e6476c5eeaf23854b14830916a67f
siderishub <56225713+siderishub@users.noreply.github.com>
2022-03-18 14:55:02 +0200

    Delete dynamic directory

--- dynamic/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'bb2a52ecc3d6a0927a7b9c5b4b5a7020');
